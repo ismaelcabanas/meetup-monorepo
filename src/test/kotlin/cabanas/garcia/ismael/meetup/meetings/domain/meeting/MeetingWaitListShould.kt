0b23b8ffc1881b7c25d68d506adbb5fb27e0d1af
Ismael Cabañas García <ismael.cabanas@adevinta.com>
2021-03-02 18:33:10 +0100

    Meeting wait list tests

---
+++ src/test/kotlin/cabanas/garcia/ismael/meetup/meetings/domain/meeting/MeetingWaitListShould.kt
@@ -0,0 +1,30 @@
+package cabanas.garcia.ismael.meetup.meetings.domain.meeting
+
+import org.junit.jupiter.api.Test
+
+class MeetingWaitListShould {
+    @Test
+    fun `not sign up member to wait list when meeting has started`() {
+        TODO("Not implemented yet")
+    }
+    
+    @Test
+    fun `not sign up member to wait list when term ended`() {
+        TODO("Not implemented yet")    
+    }
+    
+    @Test
+    fun `not sign up member to wait list when member is not a member of meeting group`() {
+        TODO("Not implemented yet")
+    }
+
+    @Test
+    fun `not sign up member to wait list when member is already in wait list`() {
+        TODO("Not implemented yet")
+    }
+
+    @Test
+    fun `sign up member to wait list successfully`() {
+        TODO("Not implemented yet")
+    }
+}
