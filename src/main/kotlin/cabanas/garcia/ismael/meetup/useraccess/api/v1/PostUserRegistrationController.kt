a8cfd7145f7f8455ce71a2745afff23de2fed04c
Ismael Cabañas García <ismael.cabanas@adevinta.com>
2021-04-22 12:37:43 +0200

    Post user registrations controller

--- src/main/kotlin/cabanas/garcia/ismael/meetup/useraccess/api/v1/PostUserRegistrationController.kt
+++ src/main/kotlin/cabanas/garcia/ismael/meetup/useraccess/api/v1/PostUserRegistrationController.kt
@@ -1,6 +1,18 @@
 package cabanas.garcia.ismael.meetup.useraccess.api.v1
 
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
 class PostUserRegistrationController {
+    @RequestMapping("/v1/user-registrations")
+    @PostMapping
+    fun execute(): ResponseEntity<Void> {
+        return ResponseEntity(HttpStatus.CREATED)
+    }
 }
 
 data class CreateUserRegistrationRequest(
